0744f594c745fb6b105da1f397713bf1db39c5cd
Saotra Fahasoavana Andrianome <saandria@c1r7s2.42antananarivo.mg>
2024-07-05 11:40:38 +0300

    rostring.c

--- success/3/rostring.c
+++ success/3/rostring.c
@@ -1,61 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rostring.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saandria <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/05 11:36:34 by saandria          #+#    #+#             */
+/*   Updated: 2024/07/05 11:37:51 by saandria         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-int		skip_whitespace(char *str, int i)
-{
-	while (str[i] == ' ' || str[i] == '\t')
-		++i;
-	return (i);
-}
-
-int		ft_wordlen(char *str)
+int main (int ac , char **av)
 {
 	int i = 0;
+	int j;
 
-	while (str[i] != '\0' && str[i] != ' ' && str[i] != '\t')
-		++i;
-	return (i);
-}
-
-int		print_word(char *str, int i, int *is_first)
-{
-	int word_len;
-
-	i = skip_whitespace(str, i);
-	word_len = ft_wordlen(str + i);
-	if (*is_first == 0)
-		write(1, " ", 1);
-	write(1, str + i, word_len);
-	*is_first = 0;
-	return (i + word_len);
-}
-
-int		epur_str(char *str)
-{
-	int i = 0;
-	int is_first = 1;
-
-	i = skip_whitespace(str, i);
-	while (str[i] != '\0')
+	if (ac >= 2)
 	{
-		i = print_word(str, i, &is_first);
-		i = skip_whitespace(str, i);
+			while (av[1][i] && (av[1][i] == ' ' || av[1][i] == '\t'))
+				i++;
+			j = i;
+			while (av[1][i])
+			{
+				while (av[1][i] && (av[1][i] != ' ' && av[1][i] != '\t'))
+					i++;
+				while (av[1][i] && (av[1][i] == ' ' || av[1][i] == '\t'))
+					i++;
+				while (av[1][i] && (av[1][i] != ' ' && av[1][i] != '\t') && (av[1][i -1] == ' ' || av[1][i - 1] == '\t'))
+				{
+					while (av[1][i] && (av[1][i] != ' ' && av[1][i] != '\t'))
+					{
+						write (1, &av[1][i], 1);
+						i++;
+					}
+					write (1, " ", 1);
+					i++;
+				}
+			}
+			while (av[1][j] && (av[1][j] != ' ' && av[1][j] != '\t'))
+			{
+				write (1, &av[1][j], 1);
+				j++;
+			}
 	}
-	return (is_first);
-}
-
-int		main(int argc, char **argv)
-{
-	if (argc >= 2)
-	{
-		char *str = argv[1];
-		int i = 0;
-		int is_first;
-
-		i = skip_whitespace(str, i);
-		i = i + ft_wordlen(str + i);
-		is_first = epur_str(str + i);
-		print_word(str, 0, &is_first);
-	}
-	write(1, "\n", 1);
-	return (0);
+	write (1, "\n", 1);
 }

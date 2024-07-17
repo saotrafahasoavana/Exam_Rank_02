9f15587bbd920d76101cddf9ab777c7176b4f3f9
saotrafahasoavana <saotraandrianome78@gmail.com>
2024-07-17 11:53:45 +0300

    rev_wstr

--- 3/rev_wstr.c
+++ 3/rev_wstr.c
@@ -1,47 +1,32 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   rev_wstr.c                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: saandria <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/27 09:55:51 by saandria          #+#    #+#             */
-/*   Updated: 2024/06/27 10:01:36 by saandria         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
-#include <stdlib.h>
 
-void	rev_wstr(char *str)
+void	reverse(char *s)
 {
-	int len;
-	int j;
-	int first_word = 1;
+	int	len = 0;
+	int	i = 0;
+	int	f = 1;
 
-	len = 0;
-	while (str[len] != '\0')
+	while (s[len])
 		len++;
 	while (len >= 0)
 	{
-		while (len >= 0 && (str[len] == '\0' || str[len] == ' ' || str[len] == '\t'))
+		while (len >= 0 && (s[len] == '\0' || s[len] == ' ' || s[len] == '\t'))
 			len--;
-		j = len;
-		while (j >= 0 && str[j] != ' ' && str[j] != '\t')
-			j--;
-		if (first_word == 0)
-			write(1, " ", 1);
-		write(1, str + j + 1, len - j);
-		first_word = 0;
-		len = j;
+		i = len;
+		while (i >= 0 && (s[i] != ' ' && s[i] != '\t'))
+			i--;
+		if (f == 0)
+			write (1, " ", 1);
+		write(1, s + i + 1, len - i);
+		f = 0;
+		len = i;
 	}
 }
 
-int		main(int argc, char **argv)
+int	main(int ac, char *av[])
 {
-	if (argc == 2)
-		rev_wstr(argv[1]);
-
+	if (ac == 2)
+		reverse(av[1]);
 	write(1, "\n", 1);
 	return (0);
 }

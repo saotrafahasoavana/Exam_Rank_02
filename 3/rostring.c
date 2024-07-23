c77fec0de690294871a96c5163abc735ba3db548
Saotra Fahasoavana Andrianome <saandria@c1r8s12.42antananarivo.mg>
2024-07-23 15:01:16 +0300

    rostring

--- 3/rostring.c
+++ 3/rostring.c
@@ -1,51 +1,37 @@
 #include <unistd.h>
 
-void write_word(char *start, char *end)
+int	main(int ac, char *av[])
 {
-    while (start < end)
-    {
-        write(1, start, 1);
-        start++;
-    }
-}
+	int	i = 0, start, end;
 
-int main(int argc, char **argv)
-{
-    char *str;
-    char *first_word_start;
-    char *first_word_end;
-    if (argc > 1)
-    {
-        str = argv[1];
-        while (*str == ' ' || *str == '\t')
-            str++;
-        first_word_start = str;
-        while (*str && *str != ' ' && *str != '\t')
-            str++;
-        first_word_end = str;
-        while (*str == ' ' || *str == '\t')
-            str++;
-        if (*str)
+	if (ac > 1)
+	{
+		while (av[1][i] && (av[1][i] == ' ' || av[1][i] == '\t'))
+			i++;
+		start = i;
+		while (av[1][i] && av[1][i] != ' ' && av[1][i] != '\t')
+			i++;
+		end = i;
+		while (av[1][i] && (av[1][i] == ' ' || av[1][i] == '\t'))
+			i++;
+		if (av[1][i])
 		{
-            while (*str)
-            {
-                if (*str == ' ' || *str == '\t')
-                {
-                    while (*str == ' ' || *str == '\t')
-                        str++;
-                    if (*str)
-                        write(1, " ", 1);
-                }
-                else
-                {
-                    write(1, str, 1);
-                    str++;
-                }
-            }
-            write(1, " ", 1);
-        }
-        write_word(first_word_start, first_word_end);
-    }
-    write(1, "\n", 1);
-    return 0;
+			while (av[1][i])
+			{
+				if (av[1][i] <= 32)
+				{
+					if (av[1][i + 1] > 32)
+						write(1, " ", 1);
+				}
+				else
+					write(1, &av[1][i], 1);
+				i++;
+			}
+			write(1, " ", 1);
+		}
+		while (start < end)
+			write(1, &av[1][start++], 1);
+	}
+	write(1, "\n", 1);
+	return (0);
 }
